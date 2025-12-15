dda24cbc92fa437f6d124f84b500bb0c335a11c4
Hrutav Modha <modhahrutav@gmail.com>
2025-12-15 15:32:34 +0530

    Added list parsing and generating in code generator

--- src/generator/index.ts
+++ src/generator/index.ts
@@ -1,10 +1,13 @@
 import type { Nodes } from '../../types/token.d.ts'
-
+import { handleList } from './utils.ts'
 export default function generate(tokens: Nodes): string {
     let htmlStr: string = ''
+    
     let i: number = 0
+
     while (i < tokens.length) {
         const token = tokens[i]
+
         switch (token?.type) {
             case 'Heading':
                 htmlStr += `<h${token.metadata?.level}>${typeof token.children?.[0] === 'object' ? generate(token?.children as any) : token.children?.[0]}</h${token.metadata?.level}>`
@@ -15,11 +18,22 @@ export default function generate(tokens: Nodes): string {
             case 'Bold':
                 htmlStr += `<b>${typeof token.children?.[0] === 'object' ? generate(token?.children as any) : token.children?.[0]}</b>`
                 break
+            case 'InlineCode':
+                htmlStr += `<code>${token.children?.[0]}</code>`
+                break
+            case 'CodeBlock':
+                htmlStr += `<pre><code>${token.children?.[0]}</code></pre>`
+                break
+            case 'UnorderedListCircle':
+                const listStr = handleList(token)
+                htmlStr += listStr
+                break
             case 'Text':
                 htmlStr += token.children?.[0]
                 break
         }
         i++
     }
+    
     return htmlStr
 }
--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,8 @@ import parse from './parser/index.ts'
 
 export default function transpile(src: string) {
     const ast = parse(src)
-    // console.log(JSON.stringify(ast, null, 4))
+    console.log(JSON.stringify(ast, null, 4))
     const htmlStr = generate(ast)
     console.log(htmlStr)
     return htmlStr
 }
-
-transpile('# __Hello World__')
